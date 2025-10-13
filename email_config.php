2307200b755b8d5bf8c864f6e09fdbff3a5803b2
RUGUMAYO <rugumayoaisha@gmail.com>
2025-10-13 10:19:57 +0300

    using group email

--- email_config.php
+++ email_config.php
@@ -1,28 +1,22 @@
 <?php
+/**
+ * Email Configuration File
+ * Settings for sending emails via Gmail SMTP
+ */
 
+// Gmail SMTP Settings
 define('MAIL_HOST', 'smtp.gmail.com');
 define('MAIL_PORT', 587);
-define('MAIL_ENCRYPTION', 'tls'); // or 'ssl' for port 465
-define('MAIL_USERNAME', 'kisha.rugus@gmail.com'); // Your Gmail address
-define('MAIL_PASSWORD', 'ucnu xkae cjpv aioo');     // Your Gmail App Password (NOT your regular password)
-define('MAIL_FROM_EMAIL', 'kisha.rugus@gmail.com');
+define('MAIL_ENCRYPTION', 'tls');
+define('MAIL_USERNAME', 'info.groupg.aits@gmail.com');
+define('MAIL_PASSWORD', 'sevd hbeg snjj lgbq');
+define('MAIL_FROM_EMAIL', 'info.groupg.aits@gmail.com');
 define('MAIL_FROM_NAME', 'Blood Donation DMS');
-
-
-define('MAIL_ENCRYPTION', '');
-define('MAIL_USERNAME', '');
-define('MAIL_PASSWORD', '');
-define('MAIL_FROM_EMAIL', 'noreply@blooddonation.local');
-define('MAIL_FROM_NAME', 'Blood Donation DMS');
-*/
 
 // Other Email Settings
 define('MAIL_REPLY_TO', MAIL_FROM_EMAIL);
 define('MAIL_CHARSET', 'UTF-8');
 
 // Email debugging (set to true for troubleshooting)
-define('MAIL_DEBUG', false); // Set to true to see detailed debug information
-
-/**
- 
+define('MAIL_DEBUG', false);
 ?>
